6844cdaf21463fba1a6c51da7aba1911e6ce0d0d
RahulCodeTiwari <rahul030704@gmail.com>
2025-02-04 23:45:56 +0530

    user authentication done

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -13,6 +13,8 @@ const userSchema = new mongoose.Schema({
             type: String,
             minlength:[3, 'last name must be at least 3 characters long'],
         },
+
+    },
         email: {
             type: String,
             required: true,
@@ -28,11 +30,11 @@ const userSchema = new mongoose.Schema({
         socketId: {
             type: String,
         }
-    }
+    
 })
 
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET)
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h'})
     return token;
 }
 
