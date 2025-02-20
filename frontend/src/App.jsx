a7d9556f8a8068769686c52ad73400990f898a37
RahulCodeTiwari <rahul030704@gmail.com>
2025-02-20 20:58:17 +0530

    update files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom'
 import Start from './pages/Start'
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
-import Captainlogin from './pages/Captainlogin'
 import CaptainSignup from './pages/CaptainSignup'
 import Home from './pages/Home'
 import UserProtectWrapper from './pages/UserProtectWrapper'
@@ -14,6 +13,7 @@ import CaptainLogout from './pages/CaptainLogout'
 import Riding from './pages/Riding'
 import CaptainRiding from './pages/CaptainRiding'
 import 'remixicon/fonts/remixicon.css'
+import CaptainLogin from './pages/CaptainLogin'
 
 const App = () => {
 
@@ -26,7 +26,7 @@ const App = () => {
         <Route path='/captain-riding' element={<CaptainRiding />} />
 
         <Route path='/signup' element={<UserSignup />} />
-        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-login' element={<CaptainLogin />} />
         <Route path='/captain-signup' element={<CaptainSignup />} />
         <Route path='/home'
           element={
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,20 +4,23 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import UserContext from './context/UserContext.jsx';
+
+import SocketProvider from './context/SocketContext.jsx';
 import CaptainContext from './context/CaptainContext.jsx';
-
-
 
 createRoot(document.getElementById('root')).render(
 
   <CaptainContext>
-    <UserContext>
-    
+  <UserContext>
+      <SocketProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-    
+      </SocketProvider>
     </UserContext>
+
   </CaptainContext>
+  
+
 
 )
